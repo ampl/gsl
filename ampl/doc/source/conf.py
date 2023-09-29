0c9b42f9ef22264d08a7b6b76015a342d599acb5
Filipe Brandão <fdabrandao@gmail.com>
2023-09-29 19:19:01 +0100

    Docs: add html_baseurl for cannonicals

--- ampl/doc/source/conf.py
+++ ampl/doc/source/conf.py
@@ -95,6 +95,8 @@ html_theme_options = {
     "logo_text": "GSL",
 }
 
+html_baseurl = "https://gsl.ampl.com"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

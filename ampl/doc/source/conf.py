76f5c35e62c7f6fc455da7fd26a403e2e3706d18
Filipe Brandão <fdabrandao@gmail.com>
2024-11-07 14:32:41 +0000

    Docs: use logo and favicon from github repo

--- ampl/doc/source/conf.py
+++ ampl/doc/source/conf.py
@@ -96,13 +96,13 @@ html_show_sphinx = False
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/logo-inline-web-v4.png"
+html_logo = "https://raw.githubusercontent.com/ampl/ampl.github.io/master/themes/static/ampl-navbar-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 # html_favicon = None
-html_favicon = "_static/cropped-favicon-raw-192x192.png"
+html_favicon = "https://raw.githubusercontent.com/ampl/ampl.github.io/master/themes/static/ampl-favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
